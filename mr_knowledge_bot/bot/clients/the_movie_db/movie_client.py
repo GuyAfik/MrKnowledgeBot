8e3a9cdf4fcfd9c5be5f237f9ee0f634e46194d8
GuyAfik <guyafik11@gmail.com>
2022-09-25 14:07:16 +0300

    add clients

---
+++ mr_knowledge_bot/bot/clients/the_movie_db/movie_client.py
@@ -0,0 +1,33 @@
+from abc import ABC
+from mr_knowledge_bot.bot.clients.base_client import parse_http_response
+from mr_knowledge_bot.bot.clients.the_movie_db.movie_db_base_client import TheMovieDBClient, poll_movies_by_page
+
+
+class MovieClient(TheMovieDBClient, ABC):
+
+    def search(self, **kwargs):
+        """
+        Searches for movies with a specific name.
+
+        Keyword Arguments:
+            movie_name (str): the movie name. (required)
+            page (int): which page should be queried. (optional)
+        """
+        if movie_name := kwargs.get('movie_name'):
+            params = {'query': movie_name}
+            if page := kwargs.get('page'):
+                params['page'] = page
+            return self.get(
+                url='/search/the_movie_db',
+                params=params
+            )
+        raise ValueError(f'The "movie_name" argument must be provided')
+
+    def discover(self, **kwargs):
+        """
+        need to add docstring with all query parameters.
+        """
+        return self.get(url='/discover/the_movie_db', params=kwargs)
+
+    def get_genres(self):
+        return self.get(url='/genre/the_movie_db/list')
---
+++ mr_knowledge_bot/bot/clients/the_movie_db/movie_db_base_client.py
@@ -0,0 +1,48 @@
+import os
+import requests
+
+from mr_knowledge_bot.bot.clients.base_client import BaseClient
+from abc import ABC, abstractmethod
+
+
+def poll_movies_by_page(func):
+    def wrapper(self, *args, **kwargs):
+        page = 1
+        all_movies = []
+        kwargs['page'] = page
+        current_movies_by_page = func(self, *args, **kwargs).get('results') or []
+        while current_movies_by_page:
+            page += 1
+            kwargs['page'] = page
+            all_movies.extend(current_movies_by_page)
+            current_movies_by_page = func(self, *args, **kwargs).get('results') or []
+        return all_movies
+
+    return wrapper
+
+
+class TheMovieDBClient(BaseClient, ABC):
+    BASE_URL = os.getenv('MOVIE_API_BASE_URL')
+
+    def __init__(self, token=None, base_url=None, verify=True):
+        super(TheMovieDBClient, self).__init__(
+            token=token or os.getenv('MOVIES_API_TOKEN'), base_url=base_url or self.BASE_URL, verify=verify
+        )
+
+    def get(self, url, params=None):
+        if not params:
+            params = {}
+        params.update({'api_key': self.token})
+        return requests.request('GET', f'{self.base_url}{url}', params=params, verify=self.verify)
+
+    @abstractmethod
+    def search(self, **kwargs):
+        pass
+
+    @abstractmethod
+    def get_genres(self):
+        pass
+
+    @abstractmethod
+    def discover(self, **kwargs):
+        pass
