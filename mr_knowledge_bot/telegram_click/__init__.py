6b5fd330ee5caa83d59967e6cbcbc3c824a18b5b
Guy Afik <53861351+GuyAfik@users.noreply.github.com>
2022-09-25 13:06:26 +0300

    add telegram-python-click (#3)

---
+++ mr_knowledge_bot/telegram_click/__init__.py
@@ -0,0 +1,49 @@
+import logging
+
+from telegram import Update
+from telegram.ext import CallbackContext
+
+from mr_knowledge_bot.telegram_click.const import *
+
+LOGGER = logging.getLogger(__name__)
+
+# global list of all commands
+COMMAND_LIST = []
+
+
+class CommandTarget:
+    """
+    Values used to specify what command target (/command@target) to accept
+    """
+    # no target is specified (meaning there is no "@" in command)
+    UNSPECIFIED = 1 << 0
+    # directly targeted at this bot
+    SELF = 1 << 1
+    # directly targeted at another bot
+    OTHER = 1 << 2
+    # any of them
+    ANY = UNSPECIFIED | SELF | OTHER
+
+
+def generate_command_list(update: Update, context: CallbackContext) -> str:
+    """
+    :return: a Markdown styled text description of all available commands
+    """
+    commands_with_permission = list(
+        filter(lambda x: x[KEY_PERMISSIONS] is None or x[KEY_PERMISSIONS].evaluate(update, context), COMMAND_LIST))
+    commands_not_hidden = list(
+        filter(lambda x: not x[KEY_HIDDEN] if isinstance(x[KEY_HIDDEN], bool)
+        else not x[KEY_HIDDEN](update, context) if callable(x[KEY_HIDDEN])
+        else True, commands_with_permission))
+    sorted_commands = sorted(commands_not_hidden, key=lambda x: (x[KEY_NAMES][0].lower(), len(x[KEY_ARGUMENTS])))
+    help_messages = list(map(lambda x: x[KEY_HELP_MESSAGE], sorted_commands))
+
+    if len(COMMAND_LIST) <= 0:
+        return "This bot does not have any commands."
+
+    if len(commands_not_hidden) <= 0:
+        return "You do not have permission to use commands."
+
+    return "\n\n".join([
+        *help_messages
+    ])
---
+++ mr_knowledge_bot/telegram_click/help.py
@@ -0,0 +1,113 @@
+from typing import List
+
+from mr_knowledge_bot.telegram_click.argument import Argument
+from mr_knowledge_bot.telegram_click.const import ARG_NAMING_PREFIXES
+from mr_knowledge_bot.telegram_click.util import escape_for_markdown
+
+
+def generate_help_message(names: [str], description: str, args: List[Argument]) -> str:
+    """
+    Generates a command usage description
+    :param names: names of the command
+    :param description: command description
+    :param args: command argument list
+    :return: help message
+    """
+    synopsis = generate_synopsis(names, args)
+
+    flags = list(filter(lambda x: x.flag, args))
+    flags = sorted(flags, key=lambda x: x.name)
+    arguments = list(filter(lambda x: not x.flag, args))
+
+    flags_description = generate_arguments_description(flags)
+    arguments_description = generate_arguments_description(arguments)
+
+    lines = [
+        synopsis,
+        "  " + description
+    ]
+    if len(flags) > 0:
+        lines.extend([
+            "Flags:",
+            flags_description
+        ])
+
+    if len(arguments) > 0:
+        lines.extend([
+            "Arguments:",
+            arguments_description
+        ])
+
+    if len(arguments) > 0 or len(flags) > 0:
+        example = generate_command_example(names, arguments, flags)
+        lines.extend([
+            "Example:",
+            "  " + example
+        ])
+
+    return "\n".join(lines)
+
+
+def generate_synopsis(names: [str], args: List[Argument]) -> str:
+    """
+    Generates the synopsis for a command
+    :param names: command names
+    :param args: arguments
+    :return:
+    """
+    command_names = list(map(lambda x: "/{}".format(escape_for_markdown(x)), names))
+    synopsis = command_names[0]
+    # append command name aliases in round brackets
+    if len(command_names) > 1:
+        synopsis += " ({})".format(", ".join(command_names[1:]))
+    # add hints about flags and arguments
+    if len(args) > 0:
+        if any(map(lambda x: x.flag, args)):
+            synopsis += " [[FLAGS]]"
+        if any(map(lambda x: not x.flag, args)):
+            synopsis += " [[ARGS]]"
+
+    return synopsis
+
+
+def generate_arguments_description(args: List[Argument]) -> str:
+    """
+    Generates the description of all given arguments
+    :param args: arguments
+    :return: description
+    """
+    argument_lines = list(map(generate_argument_description, args))
+    return "\n".join(argument_lines)
+
+
+def generate_argument_description(arg: Argument) -> str:
+    """
+    Generates the usage text for an argument
+    :param arg: the argument
+    :return: usage text line
+    """
+    arg_prefix = next(iter(ARG_NAMING_PREFIXES))
+    arg_names = list(map(lambda x: "`{}{}`".format(arg_prefix, x), arg.names))
+
+    message = "  " + ", ".join(arg_names)
+    if not arg.flag:
+        message += "\t\t`{}`".format(arg.type.__name__.upper())
+    message += "\t\t" + escape_for_markdown(arg.description)
+
+    if arg.optional and not arg.flag:
+        message += "\t(`{}`)".format(escape_for_markdown(arg.default))
+    return message
+
+
+def generate_command_example(names: List[str], arguments: List[Argument], flags: List[Argument]) -> str:
+    """
+    Generates an example call of a command
+    :param names: possible command names
+    :param arguments: command arguments (without flags)
+    :param flags: command flags
+    :return: example call
+    """
+    arg_prefix = next(iter(ARG_NAMING_PREFIXES))
+    argument_examples = list(map(lambda x: "{}".format(x.example), arguments))
+    flag_examples = list(map(lambda x: "{}{}".format(arg_prefix, x.name), flags))
+    return "`/{} {}`".format(names[0], " ".join(flag_examples + argument_examples)).strip()
