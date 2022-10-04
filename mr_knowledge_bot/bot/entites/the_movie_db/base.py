a83b0ce42154f1c197c8b0a74f1c88309f90c356
GuyAfik <guyafik11@gmail.com>
2022-10-04 13:13:38 +0300

    update entites implementation

--- mr_knowledge_bot/bot/entites/the_movie_db/base.py
+++ mr_knowledge_bot/bot/entites/the_movie_db/base.py
@@ -5,17 +5,16 @@ from mr_knowledge_bot.bot.entites.base_entity import BaseEntity
 
 class TheMovieDBBaseEntity(BaseEntity, ABC):
 
-    def __init__(self, _id, name, release_date=None, genre_ids=None, overview=None, popularity=None, rating=None):
+    def __init__(self, _id, name):
         self.id = _id
         self.name = name
-        self.release_date = release_date
-        self.genre_ids = genre_ids
-        self.overview = overview
-        self.popularity = popularity
-        self.rating = rating
 
     def to_dict(self):
         return self.__dict__
+
+    @classmethod
+    def from_response(cls, response: dict):
+        return cls(_id=response.get('id'), name=response.get('name'))
 
     def __str__(self):
         return ", ".join([f'{attr_name}={attr_value}' for attr_name, attr_value in self.to_dict().items()])
--- mr_knowledge_bot/bot/entites/the_movie_db/genre_entity.py
+++ mr_knowledge_bot/bot/entites/the_movie_db/genre_entity.py
@@ -1,10 +1,5 @@
+from mr_knowledge_bot.bot.entites.the_movie_db.base import TheMovieDBBaseEntity
 
 
-class GenreEntity:
-    def __init__(self, _id, name):
-        self.id = _id
-        self.name = name
-
-    @classmethod
-    def from_response(cls, response):
-        return cls(_id=response.get('id'), name=response.get('name'))
+class GenreEntity(TheMovieDBBaseEntity):
+    pass
